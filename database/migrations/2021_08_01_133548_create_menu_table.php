202584ceb252ee0a426c86fa72e1a6e601b332d2
Elio Martins <elio.omar.martins@hotmail.com>
2021-08-01 09:49:50 -0400

    Se agregan dos tablas, menu y menu_permisos

---
+++ database/migrations/2021_08_01_133548_create_menu_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->bigIncrements('tr_men_id');
+            $table->Char('tr_uuid');
+            $table->string('tr_men_nombre');
+            $table->string('tr_men_descripcion');
+            $table->string('tr_men_ruta');
+            $table->string('tr_men_icono');
+            $table->bigInteger('tr_men_orden')->unsigned();
+            $table->bigInteger('tr_men_id_padre')->unsigned();
+            $table->bigInteger('tr_men_est_fk')->unsigned();
+            $table->bigInteger('tr_men_vig_fk')->unsigned();
+            $table->bigInteger('tr_men_usuario_creacion')->nullable();
+            $table->bigInteger('tr_men_usuario_modificacion')->nullable();
+            $table->timestamp('tr_men_fecha_creacion')->nullable();
+            $table->timestamp('tr_men_fecha_modificaion')->nullable();
+            $table->foreign('tr_men_est_fk')->references('tr_est_id')->on('estados');
+            $table->foreign('tr_men_vig_fk')->references('tr_vig_id')->on('vigencias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
